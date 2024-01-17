225b10ec2b9b32bc79c8d02c43e9c8ffd540b8d3
Abdul Sattar Suleman <a.abdulsattarsuleman@gmail.com>
2024-01-17 18:22:49 +0500

    first day

---
+++ exercise_2.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+// Challenge 2
+// Create a program that asks the user for a number and then prints out a list of all the divisors of that number.
+
+// Solution
+
+void main() {
+  print("Enter User Input: ");
+  int? userInputNumer = int.parse(stdin.readLineSync()!);
+
+  print("Input Number is $userInputNumer");
+  for(var i =0 ; i<=)
+}
---
+++ exercises_1.dart
@@ -0,0 +1,20 @@
+// Challange 1
+//   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+// and write a program that prints out all the elements of the list that are less than 5.
+
+// Solution
+
+void main() {
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  for (var i in a) {
+    if (i < 5) {
+      print(i);
+    }
+  }
+}
+
+
+
+
+
